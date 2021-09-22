be902ccd5b672e8c8e498ec4575f6435e44ab9d3
Sourav11gcetts <souravkamilya555@gmail.com>
2021-09-22 20:17:00 +0530

    Show model class added

--- src/main/java/com/capgemini/movieticketbooking/model/Show.java
+++ src/main/java/com/capgemini/movieticketbooking/model/Show.java
@@ -1,4 +1,27 @@
 package com.capgemini.movieticketbooking.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "show")
 public class Show {
+    @Id
+    @Column(name = "id", nullable = false)
+    private int showId;
+    private LocalDateTime showEndTime;
+    private String showName;
+    private Movie movie;
+    private int screenId;
+    private int theatreId;
+
 }
